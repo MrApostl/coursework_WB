466fa833b7dc62b103a32a24adb470aa85febfcb
MrApostl <re4ickaja@gmail.com>
2024-04-21 20:15:34 +0300

    added form add card

---
+++ src/js/handlers.js
@@ -0,0 +1,29 @@
+const elemBtnAdd = document.querySelector('.add');
+const elemBtnDel = document.querySelector('.main-card-bottom_btnDel');
+
+const clickOnSeller = (event) =>{
+    event.preventDefault();
+
+    console.log(elemBtnAdd);
+    elemBtnAdd.style.display = 'block';
+    elemBtnDel.style.display = 'block';
+}
+
+const clickOnBuyer = (event) =>{
+    event.preventDefault();
+
+    
+    elemBtnAdd.style.display = 'none';
+    elemBtnDel.style.display = 'none';
+}
+
+const elemBtnFormAdd = document.querySelector('.add-card');
+
+const clickOnAddCloseCard = (event) =>{
+    event.preventDefault();
+
+    
+    elemBtnFormAdd.style.display = (elemBtnFormAdd.style.display === "none") ? "block" : "none";
+}
+
+export {clickOnSeller, clickOnBuyer, clickOnAddCloseCard}
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import Swiper from 'swiper';
+import {clickOnSeller, clickOnBuyer, clickOnAddCloseCard} from './handlers.js'
 
 const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
@@ -10,3 +11,17 @@ const swiper = new Swiper('.swiper', {
   },
 
 });
+
+
+
+const elemBtnSeller = document.querySelector('.header-wrapper__top__links__link_seller');
+const elemBtnBuyer = document.querySelector('.header-wrapper__top__links__link_buyer');
+
+elemBtnSeller.addEventListener('click', clickOnSeller);
+elemBtnBuyer.addEventListener('click', clickOnBuyer);
+
+const elemBtnOpenAddForm = document.querySelector('.add');
+const elemBtnOpenCloseForm = document.querySelector('.add-card_btn_close');
+
+elemBtnOpenAddForm.addEventListener('click', clickOnAddCloseCard);
+elemBtnOpenCloseForm.addEventListener('click', clickOnAddCloseCard);
